b9214f1a961a9899e7b02b2fc9086081a3fc12d5
Sasmita-434 <sasmitapradhan434@gmail.com>
2020-10-03 03:57:02 -0700

    Constructor updated

--- src/main/java/org/Constructor/ConstructorMain.java
+++ src/main/java/org/Constructor/ConstructorMain.java
@@ -3,16 +3,28 @@ package org.Constructor;
 public class ConstructorMain {
 
     public static void main(String[] args) {
-        int z;
+
         ConstructorReturn object1 = new ConstructorReturn();
+        ConstructorReturn object2 = new ConstructorReturn();
+        ConstructorReturn object3 = new ConstructorReturn();
 
         System.out.println("Default Constructor");
-        System.out.println("x = "+object1.x+" ; "+"y = "+object1.y);
+        System.out.println("x = "+object1.x+" ; "+"y = "+object1.y+" ; "+"z = "+object1.z);
         object1.ConstructorReturn();
+        System.out.println(object1 == object2);
+        System.out.println("Before initialization in main(), z = "+object2.z);
+        object2.z = 10;
+        System.out.println("After initialization in main(), z = "+object2.z);
+
         System.out.println("-----------------------------------------");
 
         System.out.println("Parameterized Constructor");
+        System.out.println("Before initialization,in main() z = "+object1.z);
         object1.ConstructorReturn(28,36);
+        System.out.println("After initialization, in main() z = "+object1.z);
+        System.out.println("For object2, z = "+object2.z);
+        System.out.println("For object3, z = "+object3.z);
+
         System.out.println("-----------------------------------------");
 
         System.out.println("Constructor Overloading");
--- src/main/java/org/Constructor/ConstructorReturn.java
+++ src/main/java/org/Constructor/ConstructorReturn.java
@@ -4,16 +4,23 @@ public class ConstructorReturn {
 
     int x = 21;
     int y = 32;
+    int z;
 
     public void ConstructorReturn()
     {
         System.out.println("Substraction of x & y = "+(x-y));
+        System.out.println("Before initialization, z = "+z);
+        z = 9;
+        System.out.println("After initialization, z = "+z);
     }
 
     public void ConstructorReturn(int x, int y)
     {
         System.out.println("x = "+x+" ; "+"y = "+y);
         System.out.println("Multiplication of x & y = "+(x*y));
+        System.out.println("Before initialization, z = "+z);
+        z = x;
+        System.out.println("After initialization, z = "+z);
     }
 
     public void ConstructorReturn(int x, int y, int z)
