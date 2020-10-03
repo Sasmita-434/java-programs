0670bb11a9733b7ab0f366c89587f466b3bd389a
Sasmita-434 <sasmitapradhan434@gmail.com>
2020-10-03 02:51:09 -0700

    Constructor : Default | Parameterized | Overloading

--- src/main/java/org/Constructor/ConstructorMain.java
+++ src/main/java/org/Constructor/ConstructorMain.java
@@ -10,11 +10,14 @@ public class ConstructorMain {
         System.out.println("x = "+object1.x+" ; "+"y = "+object1.y);
         object1.ConstructorReturn();
         System.out.println("-----------------------------------------");
+
         System.out.println("Parameterized Constructor");
         object1.ConstructorReturn(28,36);
         System.out.println("-----------------------------------------");
+
         System.out.println("Constructor Overloading");
         object1.ConstructorReturn(11,33,49);
+
         object1.ConstructorReturn(21,"Sasmita Pradhan");
     }
 }
--- src/main/java/org/Constructor/ConstructorReturn.java
+++ src/main/java/org/Constructor/ConstructorReturn.java
@@ -5,21 +5,25 @@ public class ConstructorReturn {
     int x = 21;
     int y = 32;
 
-    public void ConstructorReturn(){
+    public void ConstructorReturn()
+    {
         System.out.println("Substraction of x & y = "+(x-y));
     }
 
-    public void ConstructorReturn(int x, int y){
+    public void ConstructorReturn(int x, int y)
+    {
         System.out.println("x = "+x+" ; "+"y = "+y);
         System.out.println("Multiplication of x & y = "+(x*y));
     }
 
-    public void ConstructorReturn(int x, int y, int z){
+    public void ConstructorReturn(int x, int y, int z)
+    {
         System.out.println("x = "+x+" ; "+"y = "+y+" ; "+"z = "+z);
         System.out.println("Summation of x, y, & z = "+(x+y+z));
     }
 
-    public void ConstructorReturn(int roll, String name){
+    public void ConstructorReturn(int roll, String name)
+    {
         System.out.println("My Roll no. = "+roll);
         System.out.println("My Name = "+name);
     }
