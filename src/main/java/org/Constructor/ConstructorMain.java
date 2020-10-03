9bf02bf0265fa2c01864756bd33bca9ed3a9a657
Sasmita-434 <sasmitapradhan434@gmail.com>
2020-10-03 02:46:48 -0700

    Constructor - Default

---
+++ src/main/java/org/Constructor/ConstructorMain.java
@@ -0,0 +1,20 @@
+package org.Constructor;
+
+public class ConstructorMain {
+
+    public static void main(String[] args) {
+        int z;
+        ConstructorReturn object1 = new ConstructorReturn();
+
+        System.out.println("Default Constructor");
+        System.out.println("x = "+object1.x+" ; "+"y = "+object1.y);
+        object1.ConstructorReturn();
+        System.out.println("-----------------------------------------");
+        System.out.println("Parameterized Constructor");
+        object1.ConstructorReturn(28,36);
+        System.out.println("-----------------------------------------");
+        System.out.println("Constructor Overloading");
+        object1.ConstructorReturn(11,33,49);
+        object1.ConstructorReturn(21,"Sasmita Pradhan");
+    }
+}
